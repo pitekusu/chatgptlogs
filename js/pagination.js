5a5e13eedc8c21a73c2cd4a2c587f2a3378a3e85
pitekusu <pitekusu@gmail.com>
2023-06-21 13:18:13 +0900

    日時をわかりやすくした

--- js/pagination.js
+++ js/pagination.js
@@ -1,3 +1,11 @@
+// ISO 8601形式の日時を人間が読みやすい形式に変換する関数
+function formatDateTime(isoDateTime) {
+    var date = new Date(isoDateTime);
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
+    return date.toLocaleString('ja-JP', options);
+}
+
+
 $(document).ready(function() {
     $.ajax({
         url: "https://v2st81ure8.execute-api.ap-northeast-1.amazonaws.com/default/chatgptlog",
@@ -7,14 +15,19 @@ $(document).ready(function() {
             // JSONデータの配列を取得
             var items = data.Items;
 
+    	// 日時を人間が読みやすい形式に変換する
+    	items.forEach(function(item) {
+       	 item.datetime = formatDateTime(item.datetime);
+    	});
+
             // DataTableにデータをバインド
             $('#myTable').DataTable({
                 data: items,
                 columns: [
-                    { title: "日付", data: "datetime", width: "250px"},
-                    { title: "ユーザ名", data: "username", width: "250px"},
-                    { title: "質問内容", data: "usermessage"},
-                    { title: "ブッキーの答え", data: "fubukimessage" }
+                    { title: "日付", data: "datetime", width: "15%"},
+                    { title: "ユーザ名", data: "username", width: "15%"},
+                    { title: "質問内容", data: "usermessage", width: "20%"},
+                    { title: "ブッキーの答え", data: "fubukimessage", width: "50%" }
                 ],
                 responsive: true, // レスポンシブオプションを有効にする
                 order: [[0, "desc"]] // datetime列を昇順にソートする
